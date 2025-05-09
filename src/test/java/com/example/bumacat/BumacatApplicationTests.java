86f613ffd6ba28300ba935dc7b50b87b8880c5f4
pdh0128 <pdh0128a@gmail.com>
2025-05-09 19:01:56 +0900

    파일 이름 변경

---
+++ src/test/java/com/example/bumacat/BumacatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bumacat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumacatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
