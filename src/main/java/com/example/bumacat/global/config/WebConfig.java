4f7da6d7e31f52600aafca2a543c2e8057816af1
sunz8281 <sunz8281@gmail.com>
2025-07-14 10:57:47 +0900

    feat :: image 조회

--- src/main/java/com/example/bumacat/global/config/WebConfig.java
+++ src/main/java/com/example/bumacat/global/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,6 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(handlerUserArgumentResolver);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:./uploads/images/");
   }
 
   @Bean
